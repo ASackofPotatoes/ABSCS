9c940948f5d5103409e1e5c455b14c163c6dfb5a
ASackofPotatoes <tcundict@gmail.com>
2025-03-28 16:12:04 -0600

    Update context processor to only inject pageList if mission already defined

--- ABSCS/ABSCS/context_processors.py
+++ ABSCS/ABSCS/context_processors.py
@@ -1,14 +1,18 @@
 from page.models import Page
 from django.urls import reverse
-
+from django.core.cache import cache
 
 def header_data(request):
-    pages = Page.objects.all()
-    pageList = []
-    for page in pages:
-        pageList.append({
-            "title": page.title,
-            "url": reverse("view", kwargs={"id": page.id})
-        })
-    pageList = {"pageList": pageList}
-    return pageList
+    current_mission = cache.get("current_mission")
+    if current_mission:
+        pages = Page.objects.using(current_mission).all()
+        pageList = []
+        for page in pages:
+            pageList.append({
+                "title": page.title,
+                "url": reverse("view", kwargs={"id": page.id})
+            })
+        pageList = {"pageList": pageList}
+        return pageList
+    else:
+        return []
